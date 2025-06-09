81b2739af1d5651b203b5fb7115b82dd4b6b7d74
UltimateQuack <patoflamejantetv@gmail.com>
2025-06-09 17:01:27 -0300

    Adicionar arquivos de projeto.

---
+++ AskEmoji/Forms/Good.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AskEmoji.Forms
+{
+    public partial class Good : Form
+    {
+        private string imagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Res", "img", "good.png");
+
+        public Good()
+        {
+            InitializeComponent();
+            LoadImage();
+            pictureBox1.Click += PictureBox1_Click;
+        }
+
+        private void LoadImage()
+        {
+            if (File.Exists(imagePath))
+            {
+                pictureBox1.Image = Image.FromFile(imagePath);
+            }
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Selecione uma nova imagem";
+                dlg.Filter = "Imagens (*.png;*.jpg;*.jpeg;*.bmp)|*.png;*.jpg;*.jpeg;*.bmp";
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        File.Copy(dlg.FileName, imagePath, true);
+                        LoadImage();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Erro ao substituir a imagem: " + ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AskEmoji/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using AskEmoji.Forms;
+
+namespace AskEmoji
+{
+    static class Program
+    {
+        private const string ContextMenuKeyPath = @"*\shell\Ask Emoji";
+        private const string CommandKeyPath = @"*\shell\Ask Emoji\command";
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            if (args.Length == 0)
+            {
+                if (!IsContextMenuInstalled())
+                {
+                    try
+                    {
+                        InstallContextMenu();
+                        MessageBox.Show("Context menu thing installed!", "Ask Emoji", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Err while installing context menu:\n" + ex.Message, "Ask Emoji", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Already installed ;)", "Ask Emoji", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                return;
+            }
+
+            string filePath = args[0];
+            string fileName = Path.GetFileName(filePath).ToLowerInvariant();
+
+            if (fileName.Contains("good")) //if (fileName.Contains("good_app"))
+            {
+                Application.Run(new Good());
+            }
+            else if (fileName.Contains("bad")) //else if (fileName.Contains("bad_app"))
+            {
+                Application.Run(new Bad());
+            }
+            else
+            {
+                MessageBox.Show("i dont actually have an opinion on this", "Ask Emoji", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private static bool IsContextMenuInstalled()
+        {
+            using (var key = Registry.ClassesRoot.OpenSubKey(ContextMenuKeyPath))
+            {
+                return key != null;
+            }
+        }
+
+        private static void InstallContextMenu()
+        {
+            string exePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+            using (var key = Registry.ClassesRoot.CreateSubKey(ContextMenuKeyPath))
+            {
+                if (key == null)
+                    throw new Exception("Error while creating context menu key.");
+                key.SetValue(null, "Ask Emoji");
+            }
+
+            using (var commandKey = Registry.ClassesRoot.CreateSubKey(CommandKeyPath))
+            {
+                if (commandKey == null)
+                    throw new Exception("Impossible to create context menu key.");
+                commandKey.SetValue(null, $"\"{exePath}\" \"%1\"");
+            }
+        }
+    }
+}
